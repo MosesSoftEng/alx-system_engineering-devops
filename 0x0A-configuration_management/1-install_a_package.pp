dd944a2a593b07da9c2ea52cfbe2075cd19fa345
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 16:32:22 +0300

    Task 1 attempt 4 Refactor back to flask install attributes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install flask version 2.1.0 from pip3 using Puppet
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
