f5dc6b6ca2ed5b12e733af0753da2415b1657d82
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 13:48:03 +0300

    Task 1 attempt 3 Changed package to pupent-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Install flask version 2.1.0 from pip3 using Puppet
 
-package { 'Install flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
