17d02b9ed6eef9d88bb722d2e29db2602085fa59
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-16 13:29:50 +0300

    Task 0 attempt 2 Create a file school in /tmp directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create a file school in /tmp directory
 
-file { 'holberton':
+file { '/tmp/school':
   ensure  => 'file',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
+  owner   => 'www-data'
 }
