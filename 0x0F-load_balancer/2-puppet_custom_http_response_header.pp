9595232b2d5933a281f0b5f785faaf63eb3d0e0e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 11:44:49 +0300

    New puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,66 +1,37 @@
-# Install nginx web server and configure using puppet
+# puppet manifest creating a custom HTTP header response
 
-include stdlib
-
-# Update apt
-exec { 'Update apt library':
-  command => 'sudo apt update',
-  path    => '/usr/bin/'
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
 
-# Install nginx
 package { 'nginx':
-  ensure   => 'installed',
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-# Set index content
-file { '/var/www/html/index.nginx-debian.html':
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
   content => 'Hello World!',
+  require => Package['nginx'],
 }
 
-# Add custom header to nginx
-file_line { 'Custom header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen \[::\]:80 default_server;',
-  line   => "\tadd_header X-Served-By ${hostname};",
-}
-
-# Add permanent redirect
-file_line { 'Add redirect line':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
-  line   => "\tlocation /redirect_me {\n\t\treturn 301 http://github.com/;\n\t}\n",
-}
-
-# Add custom error
-file_line { 'Custom error':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
-  line   => "\terror_page 404 /custom_404.html;\n\tlocation /custom_404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}\n",
-}
-
-# Set index content
-file { '/var/www/html/custom_404.html':
-  content => "Ceci n'est pas une page",
-}
-
-# restart nginx
-exec { 'restart nginx':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-# allow HTTP
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Start nginx service
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
+
