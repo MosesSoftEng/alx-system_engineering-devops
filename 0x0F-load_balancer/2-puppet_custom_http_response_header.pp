6d78ada049aa5c6be20282c019f406055ed5483d
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 11:54:55 +0300

    Change double to single quotes

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -18,7 +18,7 @@ file_line { 'Custom header':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen [::]:80 default_server;',
-  line   => "add_header X-Served-By $hostname;",
+  line   => 'add_header X-Served-By $hostname;',
 }
 
 # Restart nginx service
