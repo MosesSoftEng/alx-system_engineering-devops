f67371e2de3f6ff7fa729183690f53b8831b0372
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 12:02:57 +0300

    Add redirect command

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -21,6 +21,14 @@ file_line { 'Custom header':
   line   => 'add_header X-Served-By $hostname;',
 }
 
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
 file { '/var/www/html/index.html':
   content => 'Hello World!',
   require => Package['nginx'],
