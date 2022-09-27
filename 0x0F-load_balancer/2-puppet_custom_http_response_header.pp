180f3e46b0e6174513455b0ba722f634b260626e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 11:49:21 +0300

    Change hostname variable

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,28 @@
-# puppet manifest creating a custom HTTP header response
+# Install nginx web server and configure using puppet
 
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+include stdlib
+
+# Update apt
+exec { 'Update apt library':
+  command => 'sudo apt update',
+  path    => '/usr/bin/'
 }
 
+# Install nginx
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+  ensure   => 'installed',
 }
 
-file_line { 'a':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
+# Add custom header to nginx
+file_line { 'Custom header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen \[::\]:80 default_server;',
+  line   => "\tadd_header X-Served-By $hostname;",
 }
 
-file_line { 'b':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
+# Restart nginx service
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
-
