80c29aa9d75b7d9fd5efdd076be2a44c518a38d4
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 10:24:05 +0300

    Task 2 attempt

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+# Install nginx web server and configure using puppet
+
+include stdlib
+
+# Update apt
+exec { 'Update apt library':
+  command => 'sudo apt update',
+  path    => '/usr/bin/'
+}
+
+# Install nginx
+package { 'nginx':
+  ensure   => 'installed',
+}
+
+# Add custom header to nginx
+file_line { 'Custom header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen \[::\]:80 default_server;',
+  line   => "\tadd_header X-Served-By ${hostname};",
+}
+
+# Restart nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
