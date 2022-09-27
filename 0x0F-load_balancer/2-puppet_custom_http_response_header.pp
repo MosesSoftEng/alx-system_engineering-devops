416ab6c9a7aeac496024a5c4313d7ef7ab374cdd
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 11:56:14 +0300

    Escape [] chars

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,7 +17,7 @@ package { 'nginx':
 file_line { 'Custom header':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen [::]:80 default_server;',
+  after  => 'listen \[::\]:80 default_server;',
   line   => 'add_header X-Served-By $hostname;',
 }
 
