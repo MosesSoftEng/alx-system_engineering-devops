3250dff1507c170b7d7605c6e7ecd99c5e9b507d
MosesSoftEng <moses.soft.eng@gmail.com>
2022-09-27 12:06:40 +0300

    Chnage commands

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,20 +11,19 @@ package { 'nginx':
   ensure   => 'installed',
 }
 
-# Add custom header to nginx
-file_line { 'Custom header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
 file_line { 'a':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 
