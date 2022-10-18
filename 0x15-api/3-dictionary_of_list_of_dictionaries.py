97fff74dc3fa81a66268b9e79699ee58858ba1c3
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-18 21:52:30 +0300

    Change JSON output format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports all users TODO list as a JSON file
+"""Exports all todos as a JSON file
 
 From https://jsonplaceholder.typicode.com/users/1/todos
 """
@@ -10,14 +10,10 @@ import sys
 # Endpoints urls
 api_url = 'https://jsonplaceholder.typicode.com/'
 users_url = api_url + 'users'
+todos_url = api_url + 'todos'
 
 
 def get_user_todos(id, username):
-    todos_url = api_url + 'users/{}/todos'.format(id)
-
-    # Request data
-    todos = requests.get(todos_url).json()
-
     """Get user's todos
     Arguments:
         id (int): user id
@@ -25,13 +21,14 @@ def get_user_todos(id, username):
     Returns:
         A list of todos
     """
-    return [
-        {
-            "username": username,
-            "task": todo.get('title'),
-            "completed": todo.get('completed'),
-        }
-        for todo in todos]
+    todos = requests.get(todos_url, params={"userId": id}).json()
+
+    return '[{}]'.format(", ".join(
+        ['{{"username": "{}", "task": "{}", "completed": {}}}'.format(username, todo.get('title'), str(todo.get('completed')).lower())
+            for todo in todos
+        ]
+        )
+    )
 
 # Disable module execution, only run directly
 if __name__ == "__main__":
@@ -41,14 +38,14 @@ if __name__ == "__main__":
 
     # Process data
     json_data = [
-        {
-            user.get('id'): [
-                get_user_todos(user.get('id'), user.get('username'))
-            ]
-        }
+        '"{}":{}'.format(user.get('id'), get_user_todos(user.get('id'), user.get('username')))
         for user in users
     ]
 
-    # Write data to file
+    json_data = json.loads("{" +", ".join(json_data) + "}")
+
+    print(json_data)
+
+    # # Write data to file
     with open('todo_all_employees.json', 'w') as json_file:
         json.dump(json_data, json_file)
