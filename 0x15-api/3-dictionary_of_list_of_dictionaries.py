d1faf5bdb335c70169698c7f5ed4344a2a3943ff
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-18 21:57:53 +0300

    Chnage JSON format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,39 +13,18 @@ users_url = api_url + 'users'
 todos_url = api_url + 'todos'
 
 
-def get_user_todos(id, username):
-    """Get user's todos
-    Arguments:
-        id (int): user id
-        username (string): user username
-    Returns:
-        A list of todos
-    """
-    todos = requests.get(todos_url, params={"userId": id}).json()
-
-    return '[{}]'.format(", ".join(
-        ['{{"username": "{}", "task": "{}", "completed": {}}}'.format(username, todo.get('title'), str(todo.get('completed')).lower())
-            for todo in todos
-        ]
-        )
-    )
-
 # Disable module execution, only run directly
 if __name__ == "__main__":
 
     # Request users data
     users = requests.get(users_url).json()
 
-    # Process data
-    json_data = [
-        '"{}":{}'.format(user.get('id'), get_user_todos(user.get('id'), user.get('username')))
-        for user in users
-    ]
-
-    json_data = json.loads("{" +", ".join(json_data) + "}")
-
-    print(json_data)
-
-    # # Write data to file
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(json_data, json_file)
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(api_url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, json_file)
