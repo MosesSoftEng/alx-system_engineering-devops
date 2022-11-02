a910d4e407b2645115c9efaaed94838d93c61621
MosesSoftEng <moses.soft.eng@gmail.com>
2022-11-02 20:04:17 +0300

    Add script to count subreddit titles with keywords

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Script to count subreddit titles with keywords."""
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Get count of subreddit titles with keywords.
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
+    """
+    # Get data
+    response = requests.request(
+        'GET',
+        'https://www.reddit.com/r/{}/hot.json?limit=100&after={}&count={}'.
+        format(subreddit, after, count),
+        headers={
+            'User-Agent': 'my browser'
+        },
+        allow_redirects=False
+    )
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
