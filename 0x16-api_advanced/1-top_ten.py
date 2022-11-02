956c3854fb7824b51c5723317df1163e3b280314
MosesSoftEng <moses.soft.eng@gmail.com>
2022-11-02 16:35:33 +0300

    Script to get subreddit top ten hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Script to get subreddit top ten hot posts'''
+import requests
+
+
+def top_ten(subreddit):
+    '''Get number of subscribers for a subreddit
+    Arguments:
+        subreddit (str): subreddit name
+    Returns:
+        number of subscribers
+    '''
+
+    # Get data
+    response = requests.request(
+        'GET',
+        'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit),
+        headers={
+            'User-Agent': 'my browser'
+        },
+        allow_redirects=False
+    )
+
+    # Check if successful
+    if response.status_code == 200:
+        # Get json data
+        try:
+            hot_posts = response.json().get('data').get('children')
+
+            [print(post.get('data').get('title')) for post in hot_posts]
+        except:
+            pass
+    else:
+        print(None)
