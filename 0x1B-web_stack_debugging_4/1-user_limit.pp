f43d43bb25f5b694113c43fd869b5291cb33c65b
MosesSoftEng <moses.soft.eng@gmail.com>
2022-11-11 13:41:43 +0300

    Add correct command path

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Increase file limit for user holberton
 exec { 'increase_file_hard_limit':
   command => 'sed -i "/holberton hard/s/holberton hard nofile 5/holberton hard nofile 1000/" /etc/security/limits.conf',
-  path    => '/usr/bin'
+  path    => '/usr/bin/:/bin/'
 }
 
 exec { 'increase_file_soft_limit':
   command => 'sed -i "/holberton soft/s/holberton soft nofile 4/holberton soft nofile 1000/" /etc/security/limits.conf',
-  path    => '/usr/bin'
+  path    => '/usr/bin/:/bin/'
 }
